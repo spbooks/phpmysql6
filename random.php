68305cf9061c2761d47fe5ec2a7c689382de780e
Tom Butler <tom@r.je>
2017-01-09 13:41:48 +0000

    Add random.php

---
+++ random.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Random Number</title>
+  </head>
+  <body>
+    <p>Generating a random number between 1 and 10: 
+      <?php
+
+      echo rand(1, 10);
+
+      ?>
+    </p>
+  </body>
+</html>
