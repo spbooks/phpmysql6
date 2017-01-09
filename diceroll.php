f40fbbbf2ea4a14a447d536ee2fd447a50fc657e
Tom Butler <tom@r.je>
2017-01-09 14:00:00 +0000

    Add diceroll.php

---
+++ diceroll.php
@@ -0,0 +1,16 @@
+<?php
+
+$roll = rand(1, 6);
+
+echo '<p>You rolled a ' . $roll . '</p>';
+
+
+if ($roll == 6) {
+  echo '<p>You win!</p>';
+}
+else {
+  echo '<p>Sorry, you didn\'t win, better luck next time!</p>';
+}
+
+echo '<p>Thanks for playing</p>';
+?>
