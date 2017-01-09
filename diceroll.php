aead9c8863a85888c7e886287763c9bbf117fb8d
Tom Butler <tom@r.je>
2017-01-09 14:23:33 +0000

    added english for second dice

--- diceroll.php
+++ diceroll.php
@@ -1,24 +1,48 @@
 <?php
 
-$english = [
-  1 => 'one',
-  2 => 'two',
-  3 => 'three',
-  4 => 'four',
-  5 => 'five',
-  6 => 'six'
-];
-
-$roll = rand(1, 6);
-
-echo '<p>You rolled a ' . $english[$roll] . '</p>';
+$roll1 = rand(1, 6);
+$roll2 = rand(1, 6);
 
 
-if ($roll == 6) {
-  echo '<p>You win!</p>';
+if ($roll1 == 1) {
+  $english = 'one';
 }
-else {
-  echo '<p>Sorry, you didn\'t win, better luck next time!</p>';
+else if ($roll1 == 2) {
+  $english = 'two';
 }
+else if ($roll1 == 3) {
+  $english = 'three';
+}
+else if ($roll1 == 4) {
+  $english = 'four';
+}
+else if ($roll1 == 5) {
+  $english = 'five';
+}
+else if ($roll1 == 6) {
+  $english = 'six';
+}
+
+if ($roll2 == 1) {
+  $englishRoll2 = 'one';
+}
+else if ($roll2 == 2) {
+  $englishRoll2 = 'two';
+}
+else if ($roll2 == 3) {
+  $englishRoll2 = 'three';
+}
+else if ($roll2 == 4) {
+  $englishRoll2 = 'four';
+}
+else if ($roll2 == 5) {
+  $englishRoll2 = 'five';
+}
+else if ($roll2 == 6) {
+  $englishRoll2 = 'six';
+}
+
+
+echo '<p>You rolled a ' . $english . ' and a ' . $englishRoll2 . '</p>';
 
 ?>
