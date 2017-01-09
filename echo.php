025617ac8d59349b844840cbeb888df5ce9c1f89
Tom Butler <tom@r.je>
2017-01-09 13:44:01 +0000

    Add echo.php

---
+++ echo.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Test page</title>
+  </head>
+  <body>
+    <p><?php echo 'This is a <strong>test</strong>!'; ?></p>
+  </body>
+</html>
