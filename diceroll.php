4e367943b07d2062b81407d1eca3770dd25fe3f1
Tom Butler <tom@r.je>
2017-01-09 14:19:43 +0000

    replaced numeral with english text

--- diceroll.php
+++ diceroll.php
@@ -1,16 +1,33 @@
 <?php
-
-do {
-  $roll = rand(1, 6);
-  echo '<p>You rolled a ' . $roll . '</p>';
+$roll = rand(1, 6);
 
 
-  if ($roll == 6) {
-    echo '<p>You win!</p>';
-  }
-  else {
-    echo '<p>Sorry, you didn\'t win, better luck next time!</p>';
-  }
+if ($roll == 1) {
+  $english = 'one';
 }
-while ($roll != 6);
+else if ($roll == 2) {
+  $english = 'two';
+}
+else if ($roll == 3) {
+  $english = 'three';
+}
+else if ($roll == 4) {
+  $english = 'four';
+}
+else if ($roll == 5) {
+  $english = 'five';
+}
+else if ($roll == 6) {
+  $english = 'six';
+}
+
+echo '<p>You rolled a ' . $english . '</p>';
+
+
+if ($roll == 6) {
+  echo '<p>You win!</p>';
+}
+else {
+  echo '<p>Sorry, you didn\'t win, better luck next time!</p>';
+}
 ?>
