c696502bf26db24733436a96686e255497a0e5c0
Tom Butler <tom@r.je>
2017-01-12 19:12:52 +0000

    changed query to UPDATE

--- public/index.php
+++ public/index.php
@@ -4,15 +4,13 @@ try {
   $pdo = new PDO('mysql:host=localhost;dbname=ijdb_sample;charset=utf8', 'ijdb_sample', 'mypassword');
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $sql = 'CREATE TABLE joke (
-        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        joketext TEXT,
-        jokedate DATE NOT NULL
-      ) DEFAULT CHARACTER SET utf8 ENGINE=InnoDB';
 
-  $pdo->exec($sql);
+  $sql = 'UPDATE joke SET jokedate="2012-04-01"
+      WHERE joketext LIKE "%programmer%"';
 
-  $output = 'Joke table successfully created.';
+  $affectedRows = $pdo->exec($sql);
+
+  $output = 'Updated ' . $affectedRows .' rows.';
 }
 catch (PDOException $e) {
   $output = 'Database error: ' . $e->getMessage();
