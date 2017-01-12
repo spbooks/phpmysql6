eb7c54047632dc4ed23b2b8c5694e5405e0373f8
Tom Butler <tom@r.je>
2017-01-12 18:19:06 +0000

    set charset/error reporting

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 try {
-  $pdo = new PDO('mysql:host=localhost;dbname=ijdb_sample', 'ijdb_sample', 'mypassword');
+  $pdo = new PDO('mysql:host=localhost;dbname=ijdb_sample;charset=utf8', 'ijdb_sample', 'mypassword');
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $output = 'Database connection established.';
 }
 catch (PDOException $e) {
