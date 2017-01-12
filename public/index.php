3a4d6979e63e214523763d2ade2ecfcdd1c5b8df
Tom Butler <tom@r.je>
2017-01-12 19:11:43 +0000

    added create query

--- public/index.php
+++ public/index.php
@@ -1,11 +1,22 @@
 <?php
+
 try {
   $pdo = new PDO('mysql:host=localhost;dbname=ijdb_sample;charset=utf8', 'ijdb_sample', 'mypassword');
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $output = 'Database connection established.';
+
+  $sql = 'CREATE TABLE joke (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        joketext TEXT,
+        jokedate DATE NOT NULL
+      ) DEFAULT CHARACTER SET utf8 ENGINE=InnoDB';
+
+  $pdo->exec($sql);
+
+  $output = 'Joke table successfully created.';
 }
 catch (PDOException $e) {
-  $output = 'Unable to connect to the database server: ' . $e;
+  $output = 'Database error: ' . $e->getMessage();
 }
 
+    
 include '../templates/output.html.php';
