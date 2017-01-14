4441410128cdb2499d3bc084c05dcd123fc659e0
Tom Butler <tom@r.je>
2017-01-14 17:33:31 +0000

    complete addjoke.php

--- public/addjoke.php
+++ public/addjoke.php
@@ -1,9 +1,21 @@
 <?php
 if (isset($_POST['joketext'])) {
   try {
-      $pdo = new PDO('mysql:host=localhost;dbname=ijdb;charset=utf8', 'ijdbuser', 'mypassword');
+      $pdo = new PDO('mysql:host=localhost;dbname=ijdb_sample;charset=utf8', 'ijdb_sample', 'mypassword');
       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+      $sql = 'INSERT INTO joke SET
+              joketext = :joketext,
+              jokedate = CURDATE()';
+
+      $stmt = $pdo->prepare($sql);
+
+      $stmt->bindValue(':joketext', $_POST['joketext']);
+
+      $stmt->execute();
+
+      header('location: jokes.php');
+      
   }
   catch (PDOException $e) {
     $title = 'An error has occurred';
