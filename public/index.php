2f75bd1b9540b79b2c436f9c8d07947a90084e93
Tom Butler <tom@r.je>
2017-01-14 16:29:52 +0000

    More detailed error message

--- public/index.php
+++ public/index.php
@@ -1,11 +1,12 @@
 <?php
 try {
-  $pdo = new PDO('mysql:host=localhost;dbname=ijdb_sample;charset=utf8', 'ijdb_sample', 'mypassword');
+  $pdo = new PDO('mysql:host=localhost;dbname=ijdb_sample;charset=utf8', 'ijdb_sample2', 'mypassword');
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $output = 'Database connection established.';
 }
 catch (PDOException $e) {
-  $output = 'Unable to connect to the database server: ' . $e;
+  $output = 'Unable to connect to the database server: ' . $e->getMessage() . ' in ' .
+  $e->getFile() . ':' . $e->getLine();
 }
 
 include '../templates/output.html.php';
