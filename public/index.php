356dbf8c2e617852c44c8202c4434ee077d26388
Tom Butler <tom@r.je>
2017-01-15 14:14:29 +0000

    update to use __DIR__

--- public/index.php
+++ public/index.php
@@ -8,4 +8,4 @@ catch (PDOException $e) {
   $output = 'Unable to connect to the database server: ' . $e;
 }
 
-include '../templates/output.html.php';
+include  __DIR__ . '/../templates/output.html.php';
