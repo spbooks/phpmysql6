f31bddaf3cca814b1daf1512ed43613e3612fbeb
Tom Butler <tom@r.je>
2017-01-15 12:54:07 +0000

    add delete joke files

--- public/deletejoke.php
+++ public/deletejoke.php
@@ -4,24 +4,20 @@ try {
   $pdo = new PDO('mysql:host=localhost;dbname=ijdb_sample;charset=utf8', 'ijdb_sample', 'mypassword');
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $sql = 'SELECT id, joketext FROM joke';
 
-  $jokes = $pdo->query($sql);
+  $sql = 'DELETE FROM joke WHERE id = :id';
+ 
+  $stmt = $pdo->prepare($sql);
 
+  $stmt->bindValue(':id', $_POST['id']);
+  $stmt->execute();
 
-  $title = 'Joke list';
-
-  ob_start();
-
-  include '../templates/jokes.html.php';
-
-  $output = ob_get_clean();
-
+  header('location: jokes.php');
 }
 catch (PDOException $e) {
   $title = 'An error has occurred';
 
-  $output = 'Database error: ' . $e->getMessage() . ' in ' .
+  $output = 'Unable to connect to the database server: ' . $e->getMessage() . ' in ' .
   $e->getFile() . ':' . $e->getLine();
 }
 
--- templates/jokes.html.php
+++ templates/jokes.html.php
@@ -1,7 +1,11 @@
 <?php foreach($jokes as $joke): ?>
 <blockquote>
   <p>
-  <?=htmlspecialchars($joke, ENT_QUOTES, 'UTF-8')?>
+  <?=htmlspecialchars($joke['joketext'], ENT_QUOTES, 'UTF-8')?>
+  <form action="deletejoke.php" method="post">
+    <input type="hidden" name="id" value="<?=$joke['id']?>">
+    <input type="submit" value="Delete">
+  </form>
   </p>
 </blockquote>
 <?php endforeach; ?>
