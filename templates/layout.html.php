e39cb35f616950502394a51f5abc18c6f65d870e
Tom Butler <tom@r.je>
2017-02-13 17:45:34 +0000

    Added CSS code to layout

--- templates/layout.html.php
+++ templates/layout.html.php
@@ -2,9 +2,14 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="jokes.css">
     <title><?=$title?></title>
   </head>
   <body>
+
+  <header>
+    <h1>Internet Joke Database</h1>
+  </header>
   <nav>
     <ul>
       <li><a href="index.php">Home</a></li>
