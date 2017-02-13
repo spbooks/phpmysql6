9b9c82ccf1ec7929100161a97cf2ceec464fe11e
Tom Butler <tom@r.je>
2017-02-13 18:33:42 +0000

    added id to sql query

--- templates/jokes.html.php
+++ templates/jokes.html.php
@@ -3,12 +3,6 @@
   <p>
   <?=htmlspecialchars($joke['joketext'], ENT_QUOTES, 'UTF-8')?>
 
-  (by <a href="mailto:<?php
-                echo htmlspecialchars($joke['email'], ENT_QUOTES,
-                    'UTF-8'); ?>"><?php
-                echo htmlspecialchars($joke['name'], ENT_QUOTES,
-                    'UTF-8'); ?></a>)
-  
   <form action="deletejoke.php" method="post">
     <input type="hidden" name="id" value="<?=$joke['id']?>">
     <input type="submit" value="Delete">
