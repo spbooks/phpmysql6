334fc9ba7055fd61a28e0d0911e5bcd556a8f198
Tom Butler <tom@r.je>
2017-02-13 17:48:42 +0000

    added css

--- templates/layout.html.php
+++ templates/layout.html.php
@@ -2,10 +2,14 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="jokes.css">
     <title><?=$title?></title>
   </head>
   <body>
   <nav>
+    <header>
+      <h1>Internet Joke Databae</h1>
+    </header>
     <ul>
       <li><a href="index.php">Home</a></li>
       <li><a href="jokes.php">Jokes List</a></li>
