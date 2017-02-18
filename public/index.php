5a00b9d2d82697afc495f7c07724fd5263e8fb13
Tom Butler <tom@r.je>
2017-02-18 16:44:26 +0000

    added backticks

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ try {
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
-  $sql = 'SELECT joketext FROM joke';
+  $sql = 'SELECT `joketext` FROM `joke`';
   $result = $pdo->query($sql);
 
   while ($row = $result->fetch()) {
