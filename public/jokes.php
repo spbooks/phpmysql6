c0ae5b481dcc8660547e0a7648d9b2f3a07f6bf0
Tom Butler <tom@r.je>
2017-02-18 16:45:56 +0000

    added backticks

--- public/jokes.php
+++ public/jokes.php
@@ -4,7 +4,7 @@ try {
   $pdo = new PDO('mysql:host=localhost;dbname=ijdb_sample;charset=utf8', 'ijdb_sample', 'mypassword');
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $sql = 'SELECT joketext FROM joke';
+  $sql = 'SELECT `joketext` FROM `joke`';
   $result = $pdo->query($sql);
 
   while ($row = $result->fetch()) {
