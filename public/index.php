dc01f9e1cb4eedd2c8e6855a0ada21d226fcd486
Tom Butler <tom@r.je>
2017-02-18 16:40:40 +0000

    added backticks

--- public/index.php
+++ public/index.php
@@ -4,10 +4,10 @@ try {
   $pdo = new PDO('mysql:host=localhost;dbname=ijdb_sample;charset=utf8', 'ijdb_sample', 'mypassword');
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $sql = 'CREATE TABLE joke (
-        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        joketext TEXT,
-        jokedate DATE NOT NULL
+  $sql = 'CREATE TABLE `joke` (
+        `id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        `joketext` TEXT,
+        `jokedate` DATE NOT NULL
       ) DEFAULT CHARACTER SET utf8 ENGINE=InnoDB';
 
   $pdo->exec($sql);
