73ecd38009932c5230c64351b4d55d0b28ac18eb
Tom Butler <tom@r.je>
2017-02-19 15:50:31 +0000

    added backticks

--- includes/DatabaseFunctions.php
+++ includes/DatabaseFunctions.php
@@ -41,5 +41,5 @@ function insertJoke($pdo, $joketext, $authorId) {
 function updateJoke($pdo, $jokeId, $joketext, $authorId) {
   $parameters = [':joketext' => $joketext, ':authorId' => $authorId, ':id' => $jokeId];
 
-  query($pdo, 'UPDATE joke SET authorId = :authorId, joketext = :joketext WHERE id = :id', $parameters);
+  query($pdo, 'UPDATE `joke` SET `authorId` = :authorId, `joketext` = :joketext WHERE `id` = :id', $parameters);
 }
