8af9aeb53380e4cc91c9dbc24d6a80dfe951ebf0
Tom Butler <tom@r.je>
2017-03-12 15:00:35 +0000

    replaced implode with rtrim

--- includes/DatabaseFunctions.php
+++ includes/DatabaseFunctions.php
@@ -56,17 +56,11 @@ function update($pdo, $table, $primaryKey, $fields) {
 	$query = ' UPDATE `' . $table .'` SET ';
 
 
-	//Start off with an empty array
-	$fieldArray = [];
-
 	foreach ($fields as $key => $value) {
-		//Add, for example, `id = :id` to the end of the array
-		$fieldArray[] = '`' . $key . '` = :' . $key;
+		$query .= '`' . $key . '` = :' . $key . ',';
 	}
 
-
-	$query .= implode(', ', $fieldArray);
-
+	$query = rtrim($query, ',');
 
 	$query .= ' WHERE `' . $primaryKey . '` = :primaryKey';
 
