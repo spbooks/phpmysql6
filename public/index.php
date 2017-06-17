78e8091e2920d62e8e2d1a053340282d2d37b1ac
Tom Butler <tom@r.je>
2017-06-17 14:54:35 +0100

    added rest of the routes

--- public/index.php
+++ public/index.php
@@ -34,6 +34,16 @@ try {
 				$controller = new JokeController($jokesTable, $authorsTable);
 				$page = $controller->home();
 			}
+			else if ($route === 'joke/edit') {
+				include __DIR__ . '/../classes/controllers/JokeController.php';
+				$controller = new JokeController($jokesTable, $authorsTable);
+				$page = $controller->edit();
+			}
+			else if ($route === 'joke/delete') {
+				include __DIR__ . '/../classes/controllers/JokeController.php';
+				$controller = new JokeController($jokesTable, $authorsTable);
+				$page = $controller->delete();
+			}
 			else if ($route === 'register') {
 				include __DIR__ . '/../classes/controllers/RegisterController.php';
 				$controller = new RegisterController($authorsTable);
