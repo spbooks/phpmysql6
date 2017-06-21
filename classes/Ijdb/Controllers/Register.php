f1176c646f729609fc5f9034e9eb160a7a4cb4e9
Tom Butler <tom@r.je>
2017-06-21 15:54:47 +0100

    added validation

--- classes/Ijdb/Controllers/Register.php
+++ classes/Ijdb/Controllers/Register.php
@@ -17,14 +17,49 @@ class Register {
 
 	public function success() {
 		return ['template' => 'registersuccess.html.php', 
-			'title' => 'Registration Successful'];
+			    'title' => 'Registration Successful'];
 	}
 
 	public function registerUser() {
 		$author = $_POST['author'];
 
-		$this->authorsTable->save($author);
+		//Assume the data is valid to begin with
+		$valid = true;
+		$errors = [];
 
-		header('Location: /author/success');
+		//But if any of the fields have been left blank, set $valid to false
+		if (empty($author['name'])) {
+			$valid = false;
+			$errors[] = 'Name cannot be blank';
+		}
+
+		if (empty($author['email'])) {
+			$valid = false;
+			$errors[] = 'Email cannot be blank';
+		}
+
+		if (empty($author['password'])) {
+			$valid = false;
+			$errors[] = 'Password cannot be blank';
+		}
+
+		//If $valid is still true, no fields were blank and the datacan be added
+		if ($valid == true) {
+			$this->authorsTable->save($author);
+
+			$this->authorsTable->save($author);
+
+			header('Location: /author/success');
+		}
+		else {
+			//If the data is not valid, show the form again
+			return ['template' => 'register.html.php', 
+				    'title' => 'Register an account',
+				    'variables' => [
+				    	'errors' => $errors,
+				    	'author' => $author
+				    ]
+				   ]; 
+		}
 	}
 }
--- templates/register.html.php
+++ templates/register.html.php
@@ -1,12 +1,31 @@
+<?php
+
+if (!empty($errors)) {
+?>
+	<div class="errors">
+		<p>Your account could not be created, please check the following:</p>
+		<ul>
+		<?php
+			foreach ($errors as $error) {
+				?>
+				<li><?= $error ?></li>
+				<?php
+			}
+		?>
+		</ul>
+	</div>
+<?php
+}
+?>
 <form action="" method="post">
     <label for="email">Your email address</label>
-    <input name="author[email]" id="email" type="text">
+    <input name="author[email]" id="email" type="text" value="<?=$author['email'] ?? ''?>">
     
     <label for="name">Your name</label>
-    <input name="author[name]" id="name" type="text">
+    <input name="author[name]" id="name" type="text" value="<?=$author['name'] ?? ''?>">
 
     <label for="password">Password</label>
-    <input name="author[password]" id="password" type="password">
+    <input name="author[password]" id="password" type="password" value="<?=$author['password'] ?? ''?>">
  
     <input type="submit" name="submit" value="Register account">
 </form>
