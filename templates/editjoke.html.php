a4ab5ea2c683bb655182c5114caefd8eb2f47bd7
Tom Butler <tom@r.je>
2017-07-22 18:57:26 +0100

    fix bug: cannot add new jokes

--- templates/editjoke.html.php
+++ templates/editjoke.html.php
@@ -1,4 +1,4 @@
-<?php if ($userId == $joke['authorId']) { ?>
+<?php if (empty($joke['id']) || $userId == $joke['authorId']) { ?>
 <form action="" method="post">
 	<input type="hidden" name="joke[id]" value="<?=$joke['id'] ?? ''?>">
     <label for="joketext">Type your joke here:</label>
