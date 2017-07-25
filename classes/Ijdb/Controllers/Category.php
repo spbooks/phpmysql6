7722b24e0036192388d56551568363fbd8524dad
Tom Butler <tom@r.je>
2017-07-25 15:00:54 +0100

    allows adding/editing categories

--- classes/Ijdb/Controllers/Category.php
+++ classes/Ijdb/Controllers/Category.php
@@ -7,4 +7,20 @@ class Category {
 	public function __construct(\Ninja\DatabaseTable $categoriesTable) {
 		$this->categoriesTable = $categoriesTable;
 	}
+
+	public function edit() {
+
+		if (isset($_GET['id'])) {
+			$category = $this->categoriesTable->findById($_GET['id']);
+		}
+
+		$title = 'Edit Category';
+
+		return ['template' => 'editcategory.html.php',
+				'title' => $title,
+				'variables' => [
+					'category' => $category ?? null
+				]
+		];
+	}
 }
--- classes/Ijdb/IjdbRoutes.php
+++ classes/Ijdb/IjdbRoutes.php
@@ -5,12 +5,14 @@ class IjdbRoutes implements \Ninja\Routes {
 	private $authorsTable;
 	private $jokesTable;
 	private $authentication;
+	private $categoriesTable;
 
 	public function __construct() {
 		include __DIR__ . '/../../includes/DatabaseConnection.php';
 
 		$this->jokesTable = new \Ninja\DatabaseTable($pdo, 'joke', 'id', '\Ijdb\Entity\Joke', [&$this->authorsTable]);
  		$this->authorsTable = new \Ninja\DatabaseTable($pdo, 'author', 'id', '\Ijdb\Entity\Author', [&$this->jokesTable]);
+ 		$this->categoriesTable = new \Ninja\DatabaseTable($pdo, 'category', 'id');
 		$this->authentication = new \Ninja\Authentication($this->authorsTable, 'email', 'password');
 	}
 
@@ -18,6 +20,7 @@ class IjdbRoutes implements \Ninja\Routes {
 		$jokeController = new \Ijdb\Controllers\Joke($this->jokesTable, $this->authorsTable, $this->authentication);
 		$authorController = new \Ijdb\Controllers\Register($this->authorsTable);
 		$loginController = new \Ijdb\Controllers\Login($this->authentication);
+		$categoryController = new \Ijdb\Controllers\Category($this->categoriesTable);
 
 		$routes = [
 			'author/register' => [
@@ -46,7 +49,6 @@ class IjdbRoutes implements \Ninja\Routes {
 					'action' => 'edit'
 				],
 				'login' => true
-				
 			],
 			'joke/delete' => [
 				'POST' => [
@@ -88,6 +90,17 @@ class IjdbRoutes implements \Ninja\Routes {
 					'controller' => $loginController,
 					'action' => 'processLogin'
 				]
+			],
+			'category/edit' => [
+				'POST' => [
+					'controller' => $categoryController,
+					'action' => 'saveEdit'
+				],
+				'GET' => [
+					'controller' => $categoryController,
+					'action' => 'edit'
+				],
+				'login' => true
 			],
 			'' => [
 				'GET' => [
