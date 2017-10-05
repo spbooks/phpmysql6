f1bf74e195f95f787560bc9725eec2c063a4e517
Tom Butler <tom@r.je>
2017-10-05 18:27:28 +0100

    updated template syntax

--- templates/register.html.php
+++ templates/register.html.php
@@ -1,22 +1,13 @@
-<?php
-
-if (!empty($errors)) {
-?>
+<?php if (!empty($errors)): ?>
 	<div class="errors">
 		<p>Your account could not be created, please check the following:</p>
 		<ul>
-		<?php
-			foreach ($errors as $error) {
-				?>
-				<li><?= $error ?></li>
-				<?php
-			}
-		?>
+		<?php foreach ($errors as $error): ?>
+			<li><?= $error ?></li>
+		<?php endforeach; 	?>
 		</ul>
 	</div>
-<?php
-}
-?>
+<?php endif; ?>
 <form action="" method="post">
     <label for="email">Your email address</label>
     <input name="author[email]" id="email" type="text" value="<?=$author['email'] ?? ''?>">
