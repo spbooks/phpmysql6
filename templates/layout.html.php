eacac9a610e8e969a0a04e375f9cf28022e3c572
wizardbear <wizardbear@naver.com>
2018-08-19 17:07:27 +0900

    translate

--- templates/layout.html.php
+++ templates/layout.html.php
@@ -8,12 +8,12 @@
   <body>
   <nav>
     <header>
-      <h1>Internet Joke Database</h1>
+      <h1>인터넷 유머 세상</h1>
     </header>
     <ul>
       <li><a href="/">Home</a></li>
-      <li><a href="/joke/list">Jokes List</a></li>
-      <li><a href="/joke/edit">Add a new Joke</a></li>
+      <li><a href="/joke/list">유머글 목록</a></li>
+      <li><a href="/joke/edit">유머글 등록/a></li>
     </ul>
   </nav>
 
