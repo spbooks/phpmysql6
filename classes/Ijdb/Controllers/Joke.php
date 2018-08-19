0d4d7201795a61b4d5eb1d103b7a6a5a946c7925
wizardbear <wizardbear@naver.com>
2018-08-19 17:07:20 +0900

    translate

--- classes/Ijdb/Controllers/Joke.php
+++ classes/Ijdb/Controllers/Joke.php
@@ -29,7 +29,7 @@ class Joke {
 		}
 
 
-		$title = 'Joke list';
+		$title = '유머글 목';
 
 		$totalJokes = $this->jokesTable->total();
 
@@ -43,7 +43,7 @@ class Joke {
 	}
 
 	public function home() {
-		$title = 'Internet Joke Database';
+		$title = '인터넷 유머 데이터베이스';
 
 		return ['template' => 'home.html.php', 'title' => $title];
 	}
@@ -69,7 +69,7 @@ class Joke {
 			$joke = $this->jokesTable->findById($_GET['id']);
 		}
 
-		$title = 'Edit joke';
+		$title = '유머글 수정';
 
 		return ['template' => 'editjoke.html.php',
 				'title' => $title,
--- public/index.php
+++ public/index.php
@@ -8,10 +8,9 @@ try {
 	$entryPoint->run();
 }
 catch (PDOException $e) {
-	$title = 'An error has occurred';
+    $title = '오류가 발생했습니다.';
 
-	$output = 'Database error: ' . $e->getMessage() . ' in ' .
-	$e->getFile() . ':' . $e->getLine();
+    $output = '데이터베이스 오류: ' . $e->getMessage() . ', 위치: ' .$e->getFile() . ':' . $e->getLine();
 
 	include  __DIR__ . '/../templates/layout.html.php';
 }
